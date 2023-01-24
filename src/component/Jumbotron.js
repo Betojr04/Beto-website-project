a05d51599b5ab4a746bd803f512f7120289554f3
Alberto Valtierra Jr <albertovaltierrajr@Albertos-MacBook-Pro.local>
2023-01-24 14:16:24 -0700

    changed some of the titles around in the jumbotron

--- src/component/Jumbotron.js
+++ src/component/Jumbotron.js
@@ -1,28 +1,14 @@
 import React from "react";
+import './Jumbotron.css';
 
 export const Jumbotron = () => {
     return (
-        <div className="jumbotron jumbotron-fluid">
-    <div className="container">
-    <h1 className="display-4">Fluid jumbotron</h1>
-    <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+            <h1 className="display-4">Welcome Superstar</h1>
+            <p className="lead">this is where superstars are made</p>
+        </div>
     </div>
-</div>
-    // <div className="jumbotron">
-    //     <h1 className="display-4">Welcome</h1>
-    //     <p className="lead">
-    //     This is testing the design of the jumbotron
-    //         </p>
-    //     <hr className="my-4" />
-    //     <p>
-    //     Something will be here about the contents of this project
-    //     </p>
-    //     <p className="lead">
-    //     <a className="btn btn-primary btn-lg" href="#" role="button">
-    //         Learn more
-    //     </a>
-    //     </p>
-    // </div>
     );
 };
 
